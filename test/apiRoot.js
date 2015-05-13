f9c1c9aafeb2a856516680875fc9d7ae0fbf0afd
shaisachs <ssachs@ngpvan.com>
2015-05-13 08:41:44 -0400

    Add tests to cover unauthorized response, and update AEP test.
    Signed-off-by: shaisachs <ssachs@ngpvan.com>

--- test/apiRoot.js
+++ test/apiRoot.js
@@ -22,9 +22,13 @@ describe('AEP', function() {
         should.equal(body.product_name, 'VAN');
         should.equal(body.osdi_version, '1.0');
         should.equal(body._links.self.href,
-          'http://ngpvan-osdi-service.herokuapp.com/api/v1/');
+          'https://ngpvan-osdi-service.herokuapp.com/api/v1/');
         should.equal(body._links.self.title,
           'NGP VAN OSDI Service Entry Point');
+
+        var tags = body._links['osdi:tags'];
+        should.equal(tags.href, 'https://ngpvan-osdi-service.herokuapp.com/api/v1/tags');
+        should.equal(tags.title, 'The collection of tags in the system');
         done();
       });
   });
