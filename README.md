02ba516ec427ad96ba33831fe5fc3c4dea55db43
Andrew Varnerin <andrew@varnerin.info>
2015-06-23 16:39:46 -0400

    add GET /api/v1/tags and dump results to response

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 Provides NGP VAN resources via the OSDI API specification.
 
+## NPM Scripts
+
+- `npm start` - start the API server
+- `npm test` - run unit tests
+- `npm run watch` - start the API server and restart when changes are made
+- `npm run coverage` - generate code coverage info from running unit tests
+
--- routes/tags.js
+++ routes/tags.js
@@ -45,7 +45,7 @@ function getOne(req, res) {
   };
 
   var success = function(activistCode) {
-    if (!activistCode || !activistCode.activistCodeId || 
+    if (!activistCode || !activistCode.activistCodeId ||
       parseInt(activistCode.activistCodeId) !== id) {
 
       return res.status(404).end();
@@ -70,8 +70,88 @@ function getOne(req, res) {
 
   var credentials = getCredentials(req);
 
-  ngpvanAPIClient.getActivistCode(vanEndpoint, 
+  ngpvanAPIClient.getActivistCode(vanEndpoint,
     credentials.apiKey, credentials.dbMode, id,
+    unauthorized, badRequest, success);
+}
+
+function getAll(req, res) {
+  var root = config.get('apiEndpoint');
+  var vanEndpoint = config.get('vanEndpoint');
+
+  var unauthorized = function() {
+    return res.status(401).end();
+  };
+
+  var badRequest = function(error) {
+    var response_code = 500;
+    if (!error) {
+      response_code = 400;
+    }
+
+    var answer = {
+      'request_type': 'atomic',
+      'response_code': response_code,
+      'resource_status': [
+        {
+          'resource': 'osd:tags',
+          'response_code': response_code,
+          'errors': [
+            {
+              'code': 'UNKNOWN',
+              'description': 'Translating VAN errors is not yet supported.'
+            }
+          ]
+        }
+      ]
+    };
+
+    return res.status(response_code).send(answer);
+  };
+
+  var success = function(activistCodes) {
+    if (!(activistCodes && activistCodes.items)) {
+      return res.status(404).end();
+    }
+
+    var answer = {
+      "total_pages": 99,
+      "per_page": 30,
+      "page": 2,
+      "total_records": 250,
+      "data": activistCodes,
+      "_links": {
+
+      },
+      "_embedded": {
+        "osdi:tags": [
+          {
+            "identifiers": [
+              "VAN:1234"
+            ],
+            "origin_system": "VAN",
+            "created_date": "blah",
+            "modified_date": "bleh",
+            "name": "Volunteers",
+            "description": "volunteers are people who volunteer",
+            "_links": {
+              "self": {
+                "href": "van_url"
+            }, "osdi:taggings":{}
+          }
+          }
+        ]
+      }
+    };
+
+    return res.status(200).send(answer);
+  };
+
+  var credentials = getCredentials(req);
+
+
+  ngpvanAPIClient.getActivistCodes(vanEndpoint,
+    credentials.apiKey, credentials.dbMode,
     unauthorized, badRequest, success);
 }
 
@@ -88,6 +168,7 @@ function getCredentials(req) {
 }
 
 module.exports = function (app) {
+  app.get('/api/v1/tags', contentType, getAll);
   app.get('/api/v1/tags/:id', contentType, getOne);
 };
 
