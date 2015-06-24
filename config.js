99165607a8f455a2e7cc56e69d091c75d00f14bd
Andrew Varnerin <andrew@varnerin.info>
2015-06-24 14:12:28 -0400

    refactoring into osdi response service

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 //Empty object on which you can set up your conrigurable options
 var cfg = {
-  
+
   // Try to default from process.env:
   // http://nodejs.org/api/process.html#process_process_env
   port : process.env.PORT || 8000,
-  
+
   apiEndpoint: 'https://ngpvan-osdi-service.herokuapp.com/api/v1/',
   vanEndpoint: 'https://api.securevan.com/v4/'
 
@@ -14,9 +14,8 @@ module.exports = {
   get: function (varName) {
     if (cfg[varName]) {
       return cfg[varName];
-    }
-    else {
-      return '';
+    } else {
+      throw new Error('Config value not found: ' + varName);
     }
   }
 };
--- routes/tags.js
+++ routes/tags.js
@@ -2,47 +2,49 @@
 var contentType = require('../middleware/contentType'),
     config = require('../config'),
     auth = require('basic-auth'),
-    ngpvanAPIClient = require('../lib/ngpvanapi-client');
+    ngpvanAPIClient = require('../lib/ngpvanapi-client'),
+    osdi = require('../lib/osdi');
+
+var root = config.get('apiEndpoint');
+var vanEndpoint = config.get('vanEndpoint');
+
+var unauthorized = function() {
+  return res.status(401).end();
+};
+
+
+function badRequest(error) {
+  var response_code = 500;
+  if (!error) {
+    response_code = 400;
+  }
+
+  var answer = {
+    'request_type': 'atomic',
+    'response_code': response_code,
+    'resource_status': [
+      {
+        'resource': 'osd:tags',
+        'response_code': response_code,
+        'errors': [
+          {
+            'code': 'UNKNOWN',
+            'description': 'Translating VAN errors is not yet supported.'
+          }
+        ]
+      }
+    ]
+  };
+
+  return res.status(response_code).send(answer);
+};
 
 function getOne(req, res) {
   var id = 0;
-  var root = config.get('apiEndpoint');
-  var vanEndpoint = config.get('vanEndpoint');
-
 
   if (req && req.params && req.params.id) {
     id = parseInt(req.params.id);
   }
-
-  var unauthorized = function() {
-    return res.status(401).end();
-  };
-
-  var badRequest = function(error) {
-    var response_code = 500;
-    if (!error) {
-      response_code = 400;
-    }
-
-    var answer = {
-      'request_type': 'atomic',
-      'response_code': response_code,
-      'resource_status': [
-        {
-          'resource': 'osd:tags',
-          'response_code': response_code,
-          'errors': [
-            {
-              'code': 'UNKNOWN',
-              'description': 'Translating VAN errors is not yet supported.'
-            }
-          ]
-        }
-      ]
-    };
-
-    return res.status(response_code).send(answer);
-  };
 
   var success = function(activistCode) {
     if (!activistCode || !activistCode.activistCodeId ||
@@ -51,19 +53,12 @@ function getOne(req, res) {
       return res.status(404).end();
     }
 
-    var answer = {
-      'identifiers': [
-        'VAN:' + activistCode.activistCodeId,
-      ],
-      'origin_system': 'VAN',
-      'name': activistCode.name,
-      'description': activistCode.description,
-      '_links': {
-        'self': {
-          'href': root + 'tags/' + id
-        },
-      }
-    };
+    var answer = osdi.createCommonItem(
+      activistCode.name,
+      activistCode.description);
+
+    osdi.addIdentifier(answer, 'VAN:' + activistCode.activistCodeId);
+    osdi.addLink(answer, 'self', 'tags/' + id);
 
     return res.status(200).send(answer);
   };
@@ -76,38 +71,6 @@ function getOne(req, res) {
 }
 
 function getAll(req, res) {
-  var root = config.get('apiEndpoint');
-  var vanEndpoint = config.get('vanEndpoint');
-
-  var unauthorized = function() {
-    return res.status(401).end();
-  };
-
-  var badRequest = function(error) {
-    var response_code = 500;
-    if (!error) {
-      response_code = 400;
-    }
-
-    var answer = {
-      'request_type': 'atomic',
-      'response_code': response_code,
-      'resource_status': [
-        {
-          'resource': 'osd:tags',
-          'response_code': response_code,
-          'errors': [
-            {
-              'code': 'UNKNOWN',
-              'description': 'Translating VAN errors is not yet supported.'
-            }
-          ]
-        }
-      ]
-    };
-
-    return res.status(response_code).send(answer);
-  };
 
   var success = function(activistCodes) {
     if (!(activistCodes && activistCodes.items)) {
