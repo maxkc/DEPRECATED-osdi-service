a83f6cf558d6de1b4150853df13ca47ac5df794a
Josh Cohen <joshco@gmail.com>
2016-03-19 12:31:17 -0400

    formatting cleanup

--- routes/events.js
+++ routes/events.js
@@ -6,6 +6,7 @@ var config = require('../config'),
 
 var Promise=require('bluebird');
 
+// Events
 function getAll(req, res) {
   var vanClient = bridge.createClient(req);
   var vanPaginationParams = bridge.getVANPaginationParams(req);
@@ -17,6 +18,7 @@ function getAll(req, res) {
     oneResourceTranslator, 'events', res);
 }
 
+// Events
 function getOne(req, res) {
   var vanClient = bridge.createClient(req);
 
@@ -31,6 +33,67 @@ function getOne(req, res) {
     'events', res);
 }
 
+
+function oneResourceTranslator(vanitem) {
+  var answer = osdi.response.createCommonItem(
+    vanitem.shortName,
+    vanitem.description);
+  answer.title = vanitem.name;
+  answer.summary = answer.description;
+
+  answer['van:event_type']=vanitem.eventType;
+  answer.start_date=vanitem.startDate;
+  answer.end_date=vanitem.endDate;
+  answer.created_date=vanitem.createdDate;
+  answer['van:shifts'] = _.map(vanitem.shifts, function(shift) {
+    return {
+      event_shift_id: shift.eventShiftId,
+      name: shift.name,
+      start_date: shift.startTime,
+      end_time: shift.endTime
+    }
+  });
+
+  if ((vanitem.locations) && (vanitem.locations.length >= 1) ) {
+    answer.location = osdi.translator.vanLocationToOSDI(vanitem.locations[0])
+  }
+
+  answer['van:locations'] = _.map(vanitem.locations, function(location) {
+    return osdi.translator.vanLocationToOSDI(location);
+  });
+
+  answer['van:roles'] = _.map(vanitem.roles, function(role) {
+    return {
+      role_id: role.roleId,
+      name: role.name,
+      is_event_lead: role.isEventLead,
+      min: role.min,
+      max: role.max,
+      goal: role.goal
+    }
+  });
+  answer['van:codes'] = _.map(vanitem.codes, function(code) {
+    return {
+      code_id: code.codeId,
+      parent_code_id: code.parentCodeId,
+      name: code.parentCodeId,
+      code_path: code.codePath,
+      created_by_name: code.createdByName,
+      created_date: code.dateCreated,
+      supported_entities: code.supportedEntities
+    }
+  });
+
+  answer['van:voter_registration_batches'] = vanitem.voterRegistrationBatches;
+  answer['van:districtFieldValue']=vanitem.districtFieldValue;
+
+  osdi.response.addIdentifier(answer, 'VAN:' + vanitem.eventId);
+  osdi.response.addSelfLink(answer, 'events', vanitem.eventId);
+  osdi.response.addLink(answer,'osdi:attendances', 'events/' + vanitem.eventId + '/attendances');
+  osdi.response.addCurie(answer, config.get('curieTemplate'));
+
+  return answer;
+}
 
 function valueOrBlank(value) {
   var answer = value;
@@ -99,32 +162,6 @@ function recordAttendance(req,res) {
   bridge.sendSingleResourceResponse(personPromise, osdi.translator.vanSignupToOSDIAttendance,
     'attendances', res);
 
-  /*
-  var rawBody = req.body;
-
-  var vanClient = bridge.createClient(req);
-
-
-
-  var person_id = parseInt(rawBody.person.identifiers[0]);
-  var event_id = parseInt(id);
-
-
-  console.dir(signup);
-
-
-  var signupResponse={
-    'status' : 'accepted',
-    'event' : id,
-    'person' : rawBody
-  };
-
-  vanClient.events.recordAttendance(signup).spread(function () {
-    return res.send(signupResponse);
-
-  })
-
-*/
 }
 
 function getAttendances(req,res) {
@@ -155,7 +192,7 @@ function getAttendance(req, res) {
   if (req && req.params && req.params.id) {
     id = req.params.id;
   }
-  //var expand = ['locations', 'codes', 'shifts', 'roles', 'notes'];
+
   var resourcePromise = vanClient.events.getAttendance(id);
 
   bridge.sendSingleResourceResponse(resourcePromise, oneAttendanceTranslator,
@@ -224,67 +261,6 @@ function signupPersonTranslator(vanitem) {
   return answer;
 
 }
-function oneResourceTranslator(vanitem) {
-  var answer = osdi.response.createCommonItem(
-    vanitem.shortName,
-    vanitem.description);
-  answer.title = vanitem.name;
-  answer.summary = answer.description;
-
-  answer['van:event_type']=vanitem.eventType;
-  answer.start_date=vanitem.startDate;
-  answer.end_date=vanitem.endDate;
-  answer.created_date=vanitem.createdDate;
-  answer['van:shifts'] = _.map(vanitem.shifts, function(shift) {
-    return {
-      event_shift_id: shift.eventShiftId,
-      name: shift.name,
-      start_date: shift.startTime,
-      end_time: shift.endTime
-    }
-  });
-
-  if ((vanitem.locations) && (vanitem.locations.length >= 1) ) {
-    answer.location = osdi.translator.vanLocationToOSDI(vanitem.locations[0])
-  }
-
-  answer['van:locations'] = _.map(vanitem.locations, function(location) {
-    return osdi.translator.vanLocationToOSDI(location);
-  });
-
-  answer['van:roles'] = _.map(vanitem.roles, function(role) {
-    return {
-      role_id: role.roleId,
-      name: role.name,
-      is_event_lead: role.isEventLead,
-      min: role.min,
-      max: role.max,
-      goal: role.goal
-    }
-  });
-  answer['van:codes'] = _.map(vanitem.codes, function(code) {
-    return {
-      code_id: code.codeId,
-      parent_code_id: code.parentCodeId,
-      name: code.parentCodeId,
-      code_path: code.codePath,
-      created_by_name: code.createdByName,
-      created_date: code.dateCreated,
-      supported_entities: code.supportedEntities
-    }
-  });
-
-  answer['van:voter_registration_batches'] = vanitem.voterRegistrationBatches;
-  answer['van:districtFieldValue']=vanitem.districtFieldValue;
-
-  osdi.response.addIdentifier(answer, 'VAN:' + vanitem.eventId);
-  osdi.response.addSelfLink(answer, 'events', vanitem.eventId);
-  osdi.response.addLink(answer,'osdi:attendances', 'events/' + vanitem.eventId + '/attendances');
-  osdi.response.addCurie(answer, config.get('curieTemplate'));
-
-  return answer;
-}
-
 module.exports = function (app) {
   app.get('/api/v1/events', getAll);
   app.get('/api/v1/events/:id', getOne);
